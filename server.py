a9d791673b97036f52c2b00379e889e017968c67
Patrick Lin <51501192+Arbalest007@users.noreply.github.com>
2023-06-10 18:54:34 -0700

    Initial Commit
    -Added Server code
    -Added sample HelloWorld.html

---
+++ server.py
@@ -0,0 +1,40 @@
+from socket import *
+import sys
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+HOST_IP = gethostbyname(gethostname())
+PORT = 1024
+
+print("Server IP is: " + HOST_IP)
+
+serverSocket.bind((HOST_IP, PORT))
+serverSocket.listen()
+
+while True:
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.readlines()
+
+        # Debugging
+        # print("The HTML file contains: ")
+        # print(outputdata)
+
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send("HTTP/1.1 404 NOT FOUND\r\n\r\n".encode())
+        connectionSocket.close()
+
+    serverSocket.close()
+    sys.exit()
