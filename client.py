b72fad8bc6007e44df1cef38ef6fb7981b8dd43e
Loc Nguyen <locdacnguyen0@gmail.com>
2023-06-13 19:19:21 -0700

    deleted most comments and put get request into one line

--- client.py
+++ client.py
@@ -10,23 +10,18 @@ if len(sys.argv) != 4:
 # client.py 'IP that was given from starting server.py' 'port number given by server.py' 'filename'
 HOST_IP = sys.argv[1]
 PORT = int(sys.argv[2])
-request = ("Get /" + sys.argv[3] + " HTTP/1.1").encode()
 
 
-#while True:
 clientSocket.connect((HOST_IP, PORT))
-clientSocket.send(request)
-# clientSocket.send(sys.argv[3])
-# clientSocket.send("Get /" + sys.argv[3].encode() + " HTTP/1.1")
+clientSocket.send(("Get /" + sys.argv[3] + " HTTP/1.1").encode())
 
 response = clientSocket.recv(1024)
 print (response)
 
 while True:
     data = clientSocket.recv(1024).decode()
-   # filename = data.split()
     
     if not data:
-      break
+        break
 
 clientSocket.close()
